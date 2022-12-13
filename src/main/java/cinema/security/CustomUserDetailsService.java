1f27adeab58d7fb7d58e07418c8457f1d3f0d6d8
Yura Hryniv <superyura444@gmail.com>
2022-12-13 20:57:34 +0200

    Implemented movie-app

---
+++ src/main/java/cinema/security/CustomUserDetailsService.java
@@ -0,0 +1,37 @@
+package cinema.security;
+
+import static org.springframework.security.core.userdetails.User.withUsername;
+
+import cinema.model.User;
+import cinema.service.UserService;
+import java.util.Optional;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private UserService userService;
+
+    public CustomUserDetailsService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> optionalUser = userService.findByEmail(username);
+        UserBuilder builder;
+        if (optionalUser.isPresent()) {
+            builder = withUsername(username);
+            builder.password(optionalUser.get().getPassword());
+            builder.roles(optionalUser.get().getRoles()
+                    .stream()
+                    .map(x -> x.getRoleName().name())
+                    .toArray(String[]::new));
+            return builder.build();
+        }
+        throw new UsernameNotFoundException("User not found");
+    }
+}
